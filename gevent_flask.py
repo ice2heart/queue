d7d8257f685ab5ce0cd22821ee6a43fe15ffd8e7
Ice2heart <ice2heart@gmail.com>
2015-03-09 11:09:31 +0500

    some works

--- gevent_flask.py
+++ gevent_flask.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
+
 from flask import Flask, jsonify, render_template, request, abort
 from gevent.pywsgi import WSGIServer
 
+from datetime import date, datetime
+
 app = Flask(__name__)
- 
+queue = []
 @app.route("/")
 def show_index():
 	return render_template('index.html')
 @app.route("/401")
 def show_404():
 	abort(404)
-@app.route('/post/<int:post_id>')
+'''@app.route('/post/<int:post_id>')
 def show_post(post_id):
-	# show the post with the given id, the id is an integer
 	return 'Post %d' % post_id
 @app.route('/tasks', methods=['GET', 'POST'])
 def get_tasks():
@@ -34,7 +36,31 @@ def get_tasks():
 
 		return jsonify(status)
 @app.route('/api/play/<int:track_id>')
-
+def get_route():
+	pass '''
+@app.route('/api/queue')
+def get_queue():
+	global queue
+	return jsonify( { 'queue': queue} )
+# "name":self.newName(), "prioriy":self.newPriority(), "date":Date() }
+@app.route('/api/add', methods=['POST'])
+def post_add():
+	global queue
+	if request.method != 'POST':
+		abort(404)
+	item = request.json['item']
+	queue.append(item)
+	print(item['date'])
+	print(datetime.fromtimestamp((item['date'])))
+	return 'ok'
+@app.route('/api/get')
+def post_get():
+	global queue
+	result = []
+	if (len(queue) == 0):
+		return jsonify( { 'item':''})
+	sortedQueue = sorted(queue, key=lambda x:x['date'])
+	return jsonify( {'item':sortedQueue[0]})
 if __name__ == "__main__":
 	app.debug = True
 	http = WSGIServer(('', 5000), app)
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-﻿function PItem(data) {
+﻿/*function PItem(data) {
 	this.fileName = ko.observable(data['file']);
     this.fileTitle = ko.observable(data['title']);
     this.fileId = ko.observable(data['id']);
@@ -15,26 +15,84 @@ function TaskListViewModel() {
     self.playStatus = ko.observableArray([]);
     self.playlist = ko.observableArray([]);
     
-    /*self.save = function() {
+    self.save = function() {
         $.ajax("/tasks", {
             data: ko.toJSON({ tasks: self.tasks }),
             type: "post", contentType: "application/json",
             success: function(result) { self.serverAnswer(result) }
         });
-    };*/
+    };
 
-    /*self.statusUpdate = function() {
+    self.statusUpdate = function() {
         $.getJSON('/api/status', function(allData) {
             self.playStatus(allData);
         });
-    }*/
+    }
     //Обновлялка состояния
     //setInterval(self.statusUpdate, 1000);
 }
-
+*/
+function BItem(data) {
+	this.id = ko.observable(data['id']);
+	this.name = ko.observable(data['name']);
+	this.date = ko.observable(data['date']);
+	this.prioriy = ko.observable(data['prioriy']);
+	this.state = ko.observable(data['state']);
+	this.text = ko.computed(function() {
+		return this.date() + ':' + this.name() + ';' + this.id() + this.prioriy() + this.state();
+	}, this);
+}
+function BQueueViewModel() {
+	var self = this;
+	self.priorityType = ['low', 'hight', 'vip'];
+	self.modes = ['add', 'queue', 'get'];
+	self.state = ['new', 'wait', 'work', 'done'];
+	self.viewMode = ko.observable(self.modes[1]);
+	self.dbqueue = ko.observableArray([]);
+	self.queueCount = ko.computed(function() {
+		return self.dbqueue().length ;
+	}, this);
+	self.info = ko.observable();
+	self.updateQueue = function () {
+		$.getJSON('/api/queue', function(allData) {
+			var mapped = $.map(allData['queue'], function(Item) {return new BItem(Item);});
+			self.dbqueue(mapped);
+		});
+		self.viewMode(self.modes[1]);
+	}
+	self.newName = ko.observable();
+	self.newPriority = ko.observable(self.priorityType[0]);
+	self.newId = ko.observable();
+	self.addData = function(){
+		data = { "name":self.newName(), "prioriy":self.newPriority(), "date":(Date.now()/1000) , "id":self.newId(), 'state':self.state[0]};
+		$.ajax('/api/add', {
+			data: ko.toJSON({'item':data}),
+			type: 'post', contentType: 'application/json',
+			success: function(result, textStatus) { console.log(result); self.info('Add done!'); self.updateQueue(); },
+			error: function(result, text, errorThrow) { self.info('add ' + text + ' '  + errorThrow); }
+		});
+	}
+	self.getItem = function(){
+		$.getJSON('/api/get', function(allData) {
+			alert('id' + allData['item']['id'] + 'name' + allData['item']['name'] + allData['item']['state']);
+		});
+	}
+	self.clickId = function (id){
+		//alert(id.id());
+		self.getItem();
+	}
+	self.vieModeUpdate = ko.computed(function() {
+		if (self.viewMode() == self.modes[1]){
+			self.updateQueue();
+        };
+		if (self.viewMode() == self.modes[2]){
+            self.getItem();
+        };
+	}, self);
+}
 //Вот этот кусок кода нужен для правильного старта нокаута
 $( document ).ready( function() {
-ko.applyBindings(new TaskListViewModel());
+ko.applyBindings(new BQueueViewModel());
 
 });
 
